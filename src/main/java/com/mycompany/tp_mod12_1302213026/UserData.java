47bcf6b76e0fadd68b2a3d51423b0f2cdbd488af
vidyaratmayantii <126158623+vidyaratmayantii@users.noreply.github.com>
2024-06-07 10:05:37 +0700

    update user data

--- src/main/java/com/mycompany/tp_mod12_1302213026/UserData.java
+++ src/main/java/com/mycompany/tp_mod12_1302213026/UserData.java
@@ -3,31 +3,39 @@ package com.mycompany.tp_mod12_1302213026;
 public class UserData {
     private int userid;
     private int password;
-    private boolean isLoggedIn;
-    
-    public UserData(int uid, int pwd){
-        this.userid = uid;
-        this.password = pwd;
+    private boolean loggedIn;
+
+    public UserData(int userid, int password) {
+        this.userid = userid;
+        this.password = password;
+        this.loggedIn = false;
     }
-    
-    public void login(){
-        int expectedPassword = 10000 - userid;
-        
-        if (password == expectedPassword) {
-            isLoggedIn = true;
-            System.out.println("Login berhasil");
-        } else {
-            try {
-                throw new LoginFailedException("failure", userid);
-            } catch (LoginFailedException ex) {
-                System.out.println("Login user " + ex.getUserid() + " gagal, telah melakukan login " + ex.getFailedCounter(userid) + " kali");
-            }
+
+    public void login() {
+        if (String.valueOf(password).length() != 4) {
+            throw new IllegalArgumentException("Password harus 4 digit");
         }
-        assert String.valueOf(userid).length() == 4 : "User ID harus 4 digit";
-        assert String.valueOf(password).length() == 4 : "Password harus 4 digit";
+        if (userid == 1234 && password == 8766) {
+            this.loggedIn = true;
+        } else {
+            throw new LoginFailedException(userid);
+        }
     }
-    
+
     public boolean isLoggedIn() {
-        return isLoggedIn;
+        return loggedIn;
+    }
+}
+
+class LoginFailedException extends RuntimeException {
+    private final int userid;
+
+    public LoginFailedException(int userid) {
+        super("Login failed for user: " + userid);
+        this.userid = userid;
+    }
+
+    public int getUserid() {
+        return userid;
     }
 }
--- src/test/java/com/mycompany/tp_mod12_1302213026/UserDataTest.java
+++ src/test/java/com/mycompany/tp_mod12_1302213026/UserDataTest.java
@@ -15,28 +15,25 @@ class UserDataTest {
     @Test
     void testUnsuccessfulLogin() {
         UserData user = new UserData(1234, 1234);
-        try {
-            user.login();
-            fail("Expected LoginFailedException to be thrown");
-        } catch (LoginFailedException e) {
-            assertEquals(1234, e.getUserid());
-        }
+        // Menggunakan assertThrows untuk mengharapkan LoginFailedException
+        LoginFailedException exception = assertThrows(LoginFailedException.class, user::login);
+        assertEquals(1234, exception.getUserid());
         assertFalse(user.isLoggedIn());
     }
 
     @Test
     void testInvalidPassword() {
         UserData user = new UserData(9999, 1);
-        Exception exception = assertThrows(IllegalArgumentException.class, user::login);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, user::login);
         assertEquals("Password harus 4 digit", exception.getMessage());
     }
 
     @Test
     void testLoginFailedException() {
         UserData user = new UserData(1234, 1234);
-        Exception exception = assertThrows(LoginFailedException.class, user::login);
+        // Menggunakan assertThrows untuk mengharapkan LoginFailedException
+        LoginFailedException exception = assertThrows(LoginFailedException.class, user::login);
         assertTrue(exception instanceof LoginFailedException);
-        LoginFailedException ex = (LoginFailedException) exception;
-        assertEquals(1234, ex.getUserid());
+        assertEquals(1234, exception.getUserid());
     }
 }
